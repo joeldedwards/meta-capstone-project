66b5d65aba996f24a9095e68456a88d154ec5fd6
jedwards <jedwards@fsx.com>
2023-10-14 11:41:02 -0400

    Base components added

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='bg-black'>
+      <img src="" alt="" />
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Reservations</a>
+        </li>
+        <li>
+          <a href="">Order Online</a>
+        </li>
+        <li>
+          <a href="">Login</a>
+        </li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header
