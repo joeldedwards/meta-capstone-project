0c73d8aa7a870d96b8dad120eeda273461b41469
jedwards <jedwards@fsx.com>
2023-10-15 17:32:23 -0400

    Testimonials added

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,10 +1,60 @@
 import React from 'react'
 
 function Testimonials() {
+
+  const customerReviews = [
+      {
+          "id": 1,
+          "name": "James Smith",
+          "img": "images/man-1.jpg",
+          "rating": 4,
+          "review": "Little Lemon is the place to go for a top-notch dining experience. The food was exquisite, and the presentation was impressive."
+      },
+      {
+          "id": 2,
+          "name": "Sarah Johnson",
+          "img": "images/woman-1.jpg",
+          "rating": 4,
+          "review": "The Little Lemon Restaurant is a hidden gem! I had the most delightful dining experience here. The food was impeccable, and the service was outstanding."
+      },
+      {
+          "id": 3,
+          "name": "David Rodriguez",
+          "img": "images/man-2.jpg",
+          "rating": 4,
+          "review": "Little Lemon's menu is a refreshing twist on classic dishes. I thoroughly enjoyed my meal, and the ambiance is perfect for a cozy dinner."
+      },
+      {
+          "id": 4,
+          "name": "Emily O'neill",
+          "img": "images/woman-2.jpg",
+          "rating": 4,
+          "review": "Little Lemon offers a unique culinary journey. I was pleasantly surprised by the fusion of flavors in their dishes."
+      }
+  ]
+
   return (
     <section id="Testimonials">
         <div className="container">
             <h1>Testimonials</h1>
+            <div className="reviews">
+              {
+                customerReviews.map((review) => (
+                  <div key={review.id} className="reviews_card">
+                    <figure>
+                      <img src={review.img} alt={review.name} />
+                    </figure>
+                    <div className="reviews_content">
+                      <div className="ratings"></div>
+                      <h3>{review.name}</h3>
+                    </div>
+                    <p>
+                      {review.review}
+                    </p>
+                  </div>
+                ))
+              }
+            </div>
         </div>
     </section>
   )
