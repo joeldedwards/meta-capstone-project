a8336ab07367a99b94f7f88071de401d5d8de86e
Joel Edwards <trinifellah@gmail.com>
2023-12-07 00:02:09 -0500

    Final commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,27 +1,38 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Nav() {
+  const [isShow, setShow] = useState(false);
+
+  const toggleClass = () => {
+    setShow(!isShow);
+  }
+
   return (
     <>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/reserve">Reservations</Link>
-        </li>
-        <li>
-          <Link to="/order">Order Online</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
-    </nav>
+      <nav>
+        <ul className={isShow ? 'show' : ''}>
+          <button type="button" onClick={toggleClass}><FontAwesomeIcon icon={faXmark} /></button>
+          <li>
+            <Link to="/" onClick={toggleClass}>Home</Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={toggleClass}>About</Link>
+          </li>
+          <li>
+            <Link to="/reserve" onClick={toggleClass}>Reservations</Link>
+          </li>
+          <li>
+            <Link to="/order" onClick={toggleClass}>Order Online</Link>
+          </li>
+          <li>
+            <Link to="/login" onClick={toggleClass}>Login</Link>
+          </li>
+        </ul>
+        <button type="button" onClick={toggleClass}><FontAwesomeIcon icon={faBars} /></button>
+      </nav>
     </>
   )
 }
